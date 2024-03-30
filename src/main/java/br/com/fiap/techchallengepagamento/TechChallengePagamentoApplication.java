1afc38a88f51eb57db890065230255df33da1447
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2024-03-30 10:00:39 -0300

    first commit

---
+++ src/main/java/br/com/fiap/techchallengepagamento/TechChallengePagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallengepagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengePagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengePagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/techchallengepagamento/TechChallengePagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallengepagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengePagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
