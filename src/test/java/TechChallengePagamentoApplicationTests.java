90a0084b2b2412d0660942155530248e600c4221
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2024-05-15 19:37:25 -0300

    feat: improving application class coverage

---
+++ src/test/java/TechChallengePagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+import br.com.fiap.techchallengepagamento.TechChallengePagamentoApplication;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest(classes = TechChallengePagamentoApplication.class)
+@ExtendWith(SpringExtension.class)
+public class TechChallengePagamentoApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
